b45965d27e735e0735eea3c16baa7f4e3e766213
Barbra2020 <bmootian@gmail.com>
2020-07-28 10:16:15 +0300

    Creating the MHAC App UI project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MapComponent } from './map/map.component';
+
+
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  },
+  {
+    path: "map",
+    component: MapComponent
+  },
+  {
+    path: "**",
+    redirectTo: "home"
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
